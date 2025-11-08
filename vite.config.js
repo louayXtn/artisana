d04b9a794863477aae456e105f07e1e6cc93be35
louayXtn <louayawadh2ec@gmail.com>
2025-11-08 23:52:49 +0100

    Update vite.config.js for static deployment

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/auth': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      },
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/auth': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false
+//       },
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     }
+//   }
+base: '/',
 });
